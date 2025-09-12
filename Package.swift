b20fff9f9ce152fc3520905c6393ab22735825b8
Bing ZHEUNG <bing@zheung.org>
2025-09-13 06:09:03 +0800

    (GitHub Actions) Use macOS 26

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 
 import PackageDescription
 
 let package = Package(
         name: "EmojiCantonese",
-        platforms: [.macOS(.v15)],
+        platforms: [.macOS(.v26)],
         products: [
                 .executable(name: "EmojiCantonese", targets: ["EmojiCantonese"])
         ],
